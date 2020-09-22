0211712c062242886594d589c99614de2d8cdeb0
rebequi <rebk1998@icloud.com>
2020-09-21 18:07:03 -0600

    filtro por checkbox

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const cards = document.querySelectorAll (".card") 
 // seleccione todas las cartas
 const buttons = document.querySelectorAll (".filtro-boton")
-console.log (buttons)
-
 const sexo = document.querySelectorAll ("input[type ='radio']")
 const filtroBusqueda = document.querySelector ('#name')
+const botonCheckbox = document.querySelectorAll ('.filtro-checkbox')
+
+console.log (botonCheckbox)
+
+// fin de los selectores
 
 filtroBusqueda.onkeydown = () => {
     console.log ("hola")
@@ -19,5 +22,34 @@ filtroBusqueda.onkeydown = () => {
         }
 
     } 
+
+// convertir todos los botones de colores a checkbox
+
+// 1. cada vez que el usuario selecicona checkbox, se filtra por ese colores
+// no tienen que funcionar todos los checkbox a la vez
+// 2, solo cuando esto este listo hacer que todos los checkbox funcionen a la vez
+
+
+
+for (let boton of botonCheckbox) {
+    botonCheckbox.onclick = () => {
+        if (botonCheckbox.checked) {
+            for (let card of cards) {
+                if (botonCheckbox.value === card.dataset.colores) {
+                    card.classList.remove('borrar')
+                }
+                else if (botonCheckbox.value ==='todos') {
+                    card.classList.remove('borrar')
+                }
+                else {
+                    card.classList.add ('borrar')
+                }
+            }
+        }
+    }
+}
+
+
+
 
 
